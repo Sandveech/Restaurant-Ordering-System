40ed13a245f394ae6757c6b9d7ae75741eeeb5d4
Sandveech <82623509+Sandveech@users.noreply.github.com>
2026-03-31 14:14:10 +0700

    Slight folder structuring

---
+++ src/main/java/Test.java
@@ -0,0 +1,9 @@
+package src.main.java;
+public class Test {
+    public static void ping() {
+        System.out.println("pong");
+    }
+    public static void main(String[] args) {
+        ping();
+    }
+}
