ad464806f5917b36a8b93e9b0735c0b2ca029ba0
Sandveech <82623509+Sandveech@users.noreply.github.com>
2026-04-07 13:35:26 +0700

    Implemented Cart class

--- src/main/java/Cart.java
+++ src/main/java/Cart.java
@@ -1,5 +1,27 @@
 package src.main.java;
+import java.util.ArrayList;
 
 public class Cart {
+    private ArrayList<OrderItem> orders;
 
+    public Cart() {
+        orders = new ArrayList<>();
+    }
+
+    // setters
+
+    // getters
+    public ArrayList<OrderItem> getOrders() {
+        return orders;
+    }
+    
+    public int getOrderCount() {
+        return orders.size();
+    }
+
+    // public
+    public Cart pushOrder(OrderItem order) {
+        if (getOrderCount() < 10) { orders.add(order); }
+        return this;
+    }
 }
--- src/main/java/RestaurantMain.java
+++ src/main/java/RestaurantMain.java
@@ -3,9 +3,17 @@ package src.main.java;
 public class RestaurantMain {
     public static void main(String[] args) {
         MenuItem carbonara = new MenuItem(0, 5, "Carbonara", "Italian cream.", "Pasta");
-        OrderItem order1 = new OrderItem(0, carbonara,1, 's');
-        order1.setQuantity(10);
+        MenuItem algio_olio = new MenuItem(0, 5, "Aglio Olio", "Garlic and oil.", "Pasta");
 
-        System.out.println(order1.calculateTotal());
+        OrderItem order1 = new OrderItem(0, carbonara,1, 's');
+
+        OrderItem order2 = new OrderItem(0, algio_olio, 2, 's');
+
+        Cart cart = new Cart();
+        cart.pushOrder(order1).pushOrder(order2);
+
+        for (int i = 0; i < cart.getOrderCount(); i++) {
+            System.out.println(cart.getOrders().get(i).getItem().getName());
+        }
     }
 }
